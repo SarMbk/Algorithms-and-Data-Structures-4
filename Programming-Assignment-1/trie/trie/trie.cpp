03f2bec172e658c3840a03853b096874db62b08c
Saruar Manarbek <sar.manar@gmail.com>
2019-08-20 17:50:31 -0400

    solved week 1 problem 1: trie

--- Programming-Assignment-1/trie/trie/trie.cpp
+++ Programming-Assignment-1/trie/trie/trie.cpp
@@ -13,32 +13,24 @@ typedef vector<edges> trie;
 
 trie build_trie(vector<string> & patterns) {
   trie t;
-  int numNodes = 1;
+  edges e;
+  t.push_back(e);
+  int index = 1;
 
   for (size_t i = 0; i< patterns.size(); i++){
       size_t curNode = 0;
       for (size_t j = 0; j< patterns[i].size(); j++){
-          char curChar = patterns[i][j];
+          char letter = patterns[i][j];
 
-          if (i==0 && j==0){
-              edges e;
-              e.emplace(curChar, numNodes);
-              t.push_back(e);
-              numNodes++;
-              curNode++;
+          if (t[curNode].count(letter)){
+              curNode = t[curNode][letter];
           }
-
-          else if (t[curNode-1].count(curChar))
-              curNode = t[curNode][curChar];
-
           else{
-              if (t.size()==curNode){
-                  edges newNode;
-                  t.push_back(newNode);
-              }
-              t[curNode].emplace(curChar, numNodes);
-              numNodes++;
-              //curNode = newNodeIndex;
+              t[curNode][letter] = index;
+              edges e;
+              t.push_back(e);
+              curNode = index;
+              index++;
           }
       }
   }
@@ -63,43 +55,4 @@ int main() {
   }
 
   return 0;
-}
-
-
-
-
-
-
-
-
-
-trie build_trie2(vector<string> & patterns) {
-  trie t;
-  for (size_t i = 0; i< patterns.size(); i++){
-      size_t curNode = 0;
-      for (size_t j = 0; j< patterns[i].size(); j++){
-          char curChar = patterns[i][j];
-
-          if (i==0 && j==0){
-              edges e;
-              t.push_back(e);
-              t[curNode].emplace(curChar, 1);
-              curNode = 1;
-          }
-
-          else if (curNode>0 && t[curNode-1].count(curChar) && !t.empty() )
-              curNode = t[curNode-1][curChar];
-          else if (curNode == 0 && t[curNode].count(curChar) && !t.empty())
-              curNode = t[curNode][curChar];
-
-          else{
-              edges newNode;
-              t.push_back(newNode);
-              int newNodeIndex = t.size();
-              t[curNode].emplace(curChar, newNodeIndex);
-              curNode = newNodeIndex;
-          }
-      }
-  }
-  return t;
 }
