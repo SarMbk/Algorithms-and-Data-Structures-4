eac00185ceb786002d991dd6eb266cfc6dfdd270
Saruar Manarbek <sar.manar@gmail.com>
2019-08-22 16:11:50 -0400

    solved week 1 problem 2: trie matching

--- Programming-Assignment-1/trie_matching/trie_matching/trie_matching.cpp
+++ Programming-Assignment-1/trie_matching/trie_matching/trie_matching.cpp
@@ -4,76 +4,96 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
 
-int const Letters =    4;
-int const NA      =   -1;
 
-struct Node
-{
-	int next [Letters];
+typedef map<char, size_t> edges;
+typedef vector<edges> trie;
 
-	Node ()
-	{
-		fill (next, next + Letters, NA);
-	}
 
-	bool isLeaf () const
-	{
-	    return (next[0] == NA && next[1] == NA && next[2] == NA && next[3] == NA);
-	}
-};
+// Function to build a trie from a vector of patterns
+trie build_trie(const vector<string> & patterns) {
+  trie t;
+  edges e;
+  t.push_back(e);
+  size_t index = 1;
 
-int letterToIndex (char letter)
-{
-	switch (letter)
-	{
-		case 'A': return 0; break;
-		case 'C': return 1; break;
-		case 'G': return 2; break;
-		case 'T': return 3; break;
-		default: assert (false); return -1;
-	}
+  for (size_t i = 0; i< patterns.size(); i++){
+      size_t curNode = 0;
+      for (size_t j = 0; j< patterns[i].size(); j++){
+          char letter = patterns[i][j];
+
+          if (t[curNode].count(letter)){
+              curNode = t[curNode][letter];
+          }
+          else{
+              t[curNode][letter] = index;
+              edges e;
+              t.push_back(e);
+              curNode = index;
+              index++;
+          }
+      }
+  }
+  return t;
 }
 
-vector <int> solve (const string& text, int n, const vector <string>& patterns)
-{
-	vector <int> result;
 
-	// write your code here
+// Algorithm implemented based on problem handout.
+void PrefixTrieMatch (const string& text, size_t n, vector<size_t> &result, trie &t){
+    size_t curNode = 0;
+    size_t curIndex = n;
 
-	return result;
+    while (true){
+        char curSymbol = text[curIndex];
+
+        if (t[curNode].empty()){
+            result.push_back(n);
+            break;
+        }
+        else if (t[curNode].count(curSymbol)){
+            curIndex++;
+            curNode = t[curNode][curSymbol];
+        }
+        else break;
+    }
+
+    return;
 }
 
-int main (void)
-{
-	string t;
-	cin >> text;
 
-	int n;
+vector<size_t> TrieMatch(const string& text, const vector<string> &patterns){
+    vector<size_t> result;
+    trie t = build_trie(patterns);
+    for (size_t i=0; i<text.size(); i++){
+        PrefixTrieMatch(text, i, result, t);
+    }
+    return result;
+}
+
+
+int main (){
+    string txt;
+    cin >> txt;
+
+    size_t n;
 	cin >> n;
 
 	vector <string> patterns (n);
-	for (int i = 0; i < n; i++)
-	{
+    for (size_t i = 0; i < n; i++)
 		cin >> patterns[i];
-	}
 
-	vector <int> ans;
-	ans = solve (t, n, patterns);
+    vector <size_t> ans;
+    ans = TrieMatch(txt, patterns);
 
-	for (int i = 0; i < (int) ans.size (); i++)
-	{
+    for (size_t i = 0; i < ans.size (); i++){
 		cout << ans[i];
-		if (i + 1 < (int) ans.size ())
-		{
+        if (i + 1 < ans.size ())
 			cout << " ";
-		}
 		else
-		{
 			cout << endl;
-		}
 	}
 
 	return 0;
