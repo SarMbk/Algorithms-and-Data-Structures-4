82c5eaf1b61837c35312f5a524d3de32f34eb480
Saruar Manarbek <sar.manar@gmail.com>
2019-10-08 18:04:33 -0400

    solved week 2 problem 1

---
+++ Programming-Assignment-2/bwt/bwt/bwt.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+using namespace std;
+
+
+// Rotates text towards left by numPos. Returns copy of rotated string.
+string leftrotate(const string &text, int numPos) {
+    string ans = text;
+    reverse(ans.begin(), ans.begin()+numPos);
+    reverse(ans.begin()+numPos, ans.end());
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+// Rotates text towards right by numPos. Returns copy of rotated string.
+string rightrotate(const string &text, int numPos) {
+    return leftrotate(text, text.length()-numPos);
+}
+
+// Makes all rotations of text and pushes them into rotations vector
+void makeBWTvector(vector<string> &rotations, const string &text){
+    for (int i=0; i<text.size(); ++i){
+        string s = rightrotate(text, i);
+        rotations.push_back(s);
+    }
+    sort(rotations.begin(), rotations.end());
+}
+
+// Returns a Burrows Wheeler transform of text.
+string BWT(const string& text) {
+  string result = "";
+  vector<string> rotations;
+  makeBWTvector(rotations, text);
+
+  int lastChar = text.size()-1;
+  for (int i=0; i<rotations.size(); ++i)
+      result+=rotations[i][lastChar];
+
+  return result;
+}
+
+
+int main() {
+  string text;
+  cin >> text;
+  cout << BWT(text) << endl;
+  return 0;
+}
