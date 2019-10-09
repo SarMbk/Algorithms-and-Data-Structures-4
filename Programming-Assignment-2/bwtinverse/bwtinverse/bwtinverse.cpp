fe2306989311b2c6e7d48b737308176508c0b516
Saruar Manarbek <sar.manar@gmail.com>
2019-10-08 20:18:44 -0400

    week 2 problem 2 in progress

--- Programming-Assignment-2/bwtinverse/bwtinverse/bwtinverse.cpp
+++ Programming-Assignment-2/bwtinverse/bwtinverse/bwtinverse.cpp
@@ -2,50 +2,84 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 
-// Rotates text towards left by numPos. Returns copy of rotated string.
-string leftrotate(const string &text, int numPos) {
-    string ans = text;
-    reverse(ans.begin(), ans.begin()+numPos);
-    reverse(ans.begin()+numPos, ans.end());
-    reverse(ans.begin(), ans.end());
-    return ans;
-}
 
-// Rotates text towards right by numPos. Returns copy of rotated string.
-string rightrotate(const string &text, int numPos) {
-    return leftrotate(text, text.length()-numPos);
-}
-
-// Makes all rotations of text and pushes them into rotations vector
-void makeBWTvector(vector<string> &rotations, const string &text){
-    for (int i=0; i<text.size(); ++i){
-        string s = rightrotate(text, i);
-        rotations.push_back(s);
+struct hash_pair {
+    size_t operator()(const pair<char, int>& p) const{
+        auto hash1 = hash<char>{}(p.first);
+        auto hash2 = hash<int>{}(p.second);
+        return hash1 ^ hash2;
     }
-    sort(rotations.begin(), rotations.end());
+};
+
+
+void populateMap (unordered_map<pair<char, int> , int, hash_pair > &firstToLast, const string &bwt){
+    // Populate hashmap with number of occurences of each character and their position in the last column
+    unordered_map<char, int> counts;
+    for (int i=0; i<bwt.size(); ++i){
+        char ch = bwt[i];
+
+        if ( !counts.count(ch) ){
+            counts.emplace(ch,1);
+            pair<char, int> charAndNum (ch, 1);
+            firstToLast.emplace(charAndNum, i);
+        }
+
+        else if ( counts.count(ch) ){
+            counts[ch]++;
+            pair<char, int> charAndNum(ch, counts[ch]);
+            firstToLast.emplace(charAndNum, i);
+        }
+    }
 }
 
-// Returns a Burrows Wheeler transform of text.
-string BWT(const string& text) {
-  string result = "";
-  vector<string> rotations;
-  makeBWTvector(rotations, text);
 
-  int lastChar = text.size()-1;
-  for (int i=0; i<rotations.size(); ++i)
-      result+=rotations[i][lastChar];
+void populateVector(vector< pair<char, int> > &firstColCounts,  const string &firstCol){
+    unordered_map<char, int> counts;
+    for (int i=0; i<firstCol.size(); ++i){
+        char ch = firstCol[i];
 
-  return result;
+        if (!counts.count(ch)){
+            pair<char, int> charAndNum(ch, 1);
+            counts.emplace(charAndNum);
+            firstColCounts.push_back(charAndNum);
+        }
+
+        else if (counts.count(ch)){
+            counts[ch]++;
+            pair<char, int> charAndNum(ch, counts[ch]);
+            firstColCounts.push_back(charAndNum);
+        }
+    }
 }
-
 
 
 string InverseBWT(const string& bwt) {
-  string text = "";
+    string text = "";
+    string firstCol = bwt;
+    sort(firstCol.begin(), firstCol.end());
+
+    // map of < <character -  instance number of character>  - character position>
+    unordered_map<pair<char, int> , int, hash_pair > firstToLast;
+    populateMap (firstToLast, bwt);
+
+    // Populate vector firstColCounts. e.g. if first col string is "abcabc" then vector is [a-1, b-1, c-1, a-2, b-2, c-2]
+    vector <pair<char, int> > firstColCounts;
+    populateVector (firstColCounts, firstCol);
+
+    for (int i=0; i<firstColCounts.size(); ++i){
+        pair<char, int> chAndNum = firstColCounts[i];
+        int nextCharPos = firstToLast.at(chAndNum);
+        char nextChar = firstCol[nextCharPos];
+        text.push_back(nextChar);
+    }
+
+
+
 
   // write your code here
 
