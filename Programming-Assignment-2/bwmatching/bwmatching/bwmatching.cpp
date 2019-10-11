cb69b2c9cbf6a58c69255d745ef9c2d07d60c4f7
Saruar Manarbek <sar.manar@gmail.com>
2019-10-11 16:12:48 -0400

    solved week 2 problem 3 but it's very slow

--- Programming-Assignment-2/bwmatching/bwmatching/bwmatching.cpp
+++ Programming-Assignment-2/bwmatching/bwmatching/bwmatching.cpp
@@ -6,14 +6,16 @@
 #include <string>
 #include <vector>
 
-using std::cin;
-using std::istringstream;
-using std::map;
-using std::string;
-using std::vector;
+//using std::cin;
+//using std::istringstream;
+//using std::map;
+//using std::string;
+//using std::vector;
 using namespace std;
 
 
+
+// This is too slow.
 
 // Preprocess the Burrows-Wheeler Transform bwt of some text
 // and compute as a result:
@@ -46,6 +48,11 @@ void PreprocessBWT(const string& bwt, map<char, int>& starts, map<char, vector<i
       }
   }
 
+  for (map<char, vector<int> > :: iterator it = counts.begin(); it!=counts.end(); ++it){
+      char symbol = it->first;
+      counts[symbol].insert(counts[symbol].begin(), 0);
+  }
+
   //Populate the starts map
   string text = bwt;
   sort(text.begin(), text.end());
@@ -55,90 +62,91 @@ void PreprocessBWT(const string& bwt, map<char, int>& starts, map<char, vector<i
           starts.emplace(symbol, i);
       }
   }
-
 }
-
-
-
-
-
 
 
 
 // Compute the number of occurrences of string pattern in the text
 // given only Burrows-Wheeler Transform bwt of the text and additional
 // information we get from the preprocessing stage - starts and occ_counts_before.
-int CountOccurrences(const string& pattern, 
+int CountOccurrences(string& pattern,
                      const string& bwt, 
-                     const map<char, int>& starts, 
-                     const map<char, vector<int> >& counts) {
-  // Implement this function yourself
+                     map<char, int>& starts,
+                     map<char, vector<int> >& counts) {
+    int top = 0;
+    int bottom = bwt.size()-1;
+
+    while (top<=bottom){
+        if ( !pattern.empty() ){
+            char symbol = pattern[pattern.size()-1];
+            pattern.pop_back();
+            if (!starts.count(symbol))
+                return  0;
+            top = starts[symbol] + counts[symbol][top];
+            bottom = starts[symbol] + counts[symbol][bottom+1] - 1;
+        }
+        else
+            return bottom - top + 1;
+    }
+
+    return 0;
+}
+
+
+
+int main() {
+  string bwt;
+  cin >> bwt;
+  int pattern_count;
+  cin >> pattern_count;
+
+  // Start of each character in the sorted list of characters of bwt,
+  // see the description in the comment about function PreprocessBWT
+  map<char, int> starts;
+
+  // Occurrence counts for each character and each position in bwt,
+  // see the description in the comment about function PreprocessBWT
+  map<char, vector<int> > counts;
+
+  // Preprocess the BWT once to get starts and occ_count_before.
+  // For each pattern, we will then use these precomputed values and
+  // spend only O(|pattern|) to find all occurrences of the pattern
+  // in the text instead of O(|pattern| + |text|).
+  PreprocessBWT(bwt, starts, counts);
+  for (int pi = 0; pi < pattern_count; ++pi) {
+    string pattern;
+    cin >> pattern;
+    int occ_count = CountOccurrences(pattern, bwt, starts, counts);
+    printf("%d ", occ_count);
+  }
+  printf("\n");
   return 0;
 }
 
 
 
 
-
-
-
-
-
-
+// Internal main method for testing and debugging
+/*
 int main(){
-    string bwt = "smnpbnnaaaaa$a";
+    string bwt;
+    string pattern;
+    cin >> bwt;
+    cin >> pattern;
+
+
+    string bwt = "SMNPBNNAAAAA$A";
+    string pattern = "ANA";
+    string bwt = "AGGGAA$";
+    string pattern = "GA";
+
+
     map<char,int> starts;
     map<char, vector<int> > counts;
     PreprocessBWT(bwt,starts,counts);
+    int numMatches = CountOccurrences(pattern, bwt, starts, counts);
+    cout << numMatches << endl;
+
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//int main() {
-//  string bwt;
-//  cin >> bwt;
-//  int pattern_count;
-//  cin >> pattern_count;
-
-//  // Start of each character in the sorted list of characters of bwt,
-//  // see the description in the comment about function PreprocessBWT
-//  map<char, int> starts;
-
-//  // Occurrence counts for each character and each position in bwt,
-//  // see the description in the comment about function PreprocessBWT
-//  map<char, vector<int> > counts;
-
-//  // Preprocess the BWT once to get starts and occ_count_before.
-//  // For each pattern, we will then use these precomputed values and
-//  // spend only O(|pattern|) to find all occurrences of the pattern
-//  // in the text instead of O(|pattern| + |text|).
-//  PreprocessBWT(bwt, starts, counts);
-//  for (int pi = 0; pi < pattern_count; ++pi) {
-//    string pattern;
-//    cin >> pattern;
-//    int occ_count = CountOccurrences(pattern, bwt, starts, counts);
-//    printf("%d ", occ_count);
-//  }
-//  printf("\n");
-//  return 0;
-//}
+*/
